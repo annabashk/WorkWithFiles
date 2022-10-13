db4097ad5e1dc398383c16c6c2e87f2a972c9b61
Анна Башкатова <annabashk13@gmail.com>
2022-10-13 22:14:43 +0300

    Задание 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+Console.WriteLine("Введите путь:");
+float Size = SizeCatalog(Console.ReadLine());
+Console.WriteLine("Размер указанной папки: " + Size + " байт");
+
+static float SizeCatalog(string folder)
+{
+        DirectoryInfo di = new DirectoryInfo(folder);
+        DirectoryInfo[] diA = di.GetDirectories();
+        FileInfo[] fi = di.GetFiles();
+        float FileSize = 0.0f;
+
+        foreach (FileInfo file in fi)
+        {
+            FileSize += file.Length;
+        }
+
+        foreach (DirectoryInfo dir in diA)
+        {
+            FileSize += SizeCatalog(dir.FullName);
+        }
+    return FileSize;
+}
