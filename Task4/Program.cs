70c59bddbee68dcbb3616c73ef7cdbd31c55f899
Анна Башкатова <annabashk13@gmail.com>
2022-10-13 22:53:32 +0300

    Начало выполнения задания 4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+[Serializable]
+class Student
+{
+    public string Name { get; set; }    
+    public string Group { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public Student(string name, string group, DateTime dateOfBirth)
+    {
+        Name = name;
+        Group = group;
+        DateOfBirth = dateOfBirth;
+    }
+}
